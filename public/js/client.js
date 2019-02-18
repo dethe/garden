57e712bf00b35094d4b284d382288392d445788e
Dethe Elza <dethe@livingcode.org>
2019-02-17 23:52:10 -0800

    move all library files from CDN to local

--- public/js/client.js
+++ public/js/client.js
@@ -1,25 +1,33 @@
+const socket = io();
 const app = feathers();
+app.configure(feathers.socketio(socket));
+app.configure(feathers.authentication({
+  storage: window.localStorage
+}));
 
-// Register a simple todo service that return the name and a text
-app.use('todos', {
-  async get(name) {
-    // Return an object in the form of { name, text }
-    return {
-      name,
-      text: `You have to do ${name}`
-    };
-  }
-});
+$('#login-button')._.bind('click', evt => $('#login-form').removeAttribute('hidden'));
 
-// A function that gets and logs a todo from the service
-async function logTodo(name) {
-  // Get the service we registered above
-  const service = app.service('todos');
-  // Call the `get` method with a name
-  const todo = await service.get(name);
 
-  // Log the todo we got back
-  console.log(todo);
-}
+// // Register a simple todo service that return the name and a text
+// app.use('todos', {
+//   async get(name) {
+//     // Return an object in the form of { name, text }
+//     return {
+//       name,
+//       text: `You have to do ${name}`
+//     };
+//   }
+// });
 
-logTodo('dishes');
+// // A function that gets and logs a todo from the service
+// async function logTodo(name) {
+//   // Get the service we registered above
+//   const service = app.service('todos');
+//   // Call the `get` method with a name
+//   const todo = await service.get(name);
+
+//   // Log the todo we got back
+//   console.log(todo);
+// }
+
+// logTodo('dishes');
