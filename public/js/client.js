d7bbeae3e521ccd453f49eadc45a46bb41146067
Dethe Elza <dethe@livingcode.org>
2019-02-21 14:04:14 -0800

    login working

--- public/js/client.js
+++ public/js/client.js
@@ -5,29 +5,98 @@ app.configure(feathers.authentication({
   storage: window.localStorage
 }));
 
-$('#login-button')._.bind('click', evt => $('#login-form').removeAttribute('hidden'));
+function showLogin(evt){
+  $('#login-form').removeAttribute('hidden');
+  $('#signup-form').setAttribute('hidden', '');
+}
+
+function showSignup(evt){
+  $('#signup-form').removeAttribute('hidden');
+  $('#login-form').setAttribute('hidden', '');
+}
+
+$('#login-button')._.bind('click', showLogin);
+$('#signup-button')._.bind('click', showSignup);
+$('#logout-button')._.bind('click', evt => app.logout());
+
+// Retrieve email/password object from the login/signup page
+const getLoginCredentials = () => {
+  const user = {
+    email: document.querySelector('#login-email').value,
+    passphrase: document.querySelector('#login-password').value
+  };
+  return user;
+};
+
+const getSignupCredentials = () => {
+  const user = {
+    name: document.querySelector('#signup-email').value,
+    email: document.querySelector('#signup-email').value,
+    passphrase: document.querySelector('#signup-password').value
+  };
+  return user;
+}
+
+// Log in either using the given email/password or the token from storage
+const login = async credentials => {
+  try {
+    if(!credentials) {
+      // Try to authenticate using the JWT from localStorage
+      await app.authenticate();
+    } else {
+      // If we get login information, add the strategy we want to use for login
+      const payload = Object.assign({ strategy: 'local' }, credentials);
+
+      await app.authenticate(payload);
+    }
+  } catch(error) {
+    console.log('authentication error: %o', error);
+ }
+};
+
+// sign up and log in at the same time
+const signup = async credentials => {
+  await app.service('users').create(credentials);
+  await login(credentials);
+};
 
 
-// // Register a simple todo service that return the name and a text
-// app.use('todos', {
-//   async get(name) {
-//     // Return an object in the form of { name, text }
-//     return {
-//       name,
-//       text: `You have to do ${name}`
-//     };
-//   }
-// });
+const state = {
 
-// // A function that gets and logs a todo from the service
-// async function logTodo(name) {
-//   // Get the service we registered above
-//   const service = app.service('todos');
-//   // Call the `get` method with a name
-//   const todo = await service.get(name);
+};
 
-//   // Log the todo we got back
-//   console.log(todo);
-// }
 
-// logTodo('dishes');
+$('#login-action')._.bind('click', evt => login(getLoginCredentials()));
+$('#signup-action')._.bind('click', evt => signup(getSignupCredentials()));
+
+function authenticated(response){
+  $('#login-form').setAttribute('hidden', '');
+  $('#signup-form').setAttribute('hidden', '');
+  $('#login-button-ui').setAttribute('hidden', '');
+  $('#signup-button-ui').setAttribute('hidden', '');
+  $('#logout-button-ui').removeAttribute('hidden');
+
+  console.log('Authenticated: %o', response);
+  app.service('worlds').find({}).then(worlds => {console.log('Worlds: %o', worlds); state.worlds = worlds});
+}
+
+function loggedOut(response){
+  console.log('log out: %o', response);
+  $('#login-button-ui').removeAttribute('hidden');
+  $('#signup-button-ui').removeAttribute('hidden');
+  $('#logout-button-ui').setAttribute('hidden', '');
+}
+
+function addWorld(world){
+  console.log('add world %o', world);
+}
+
+app.service('worlds').on('created', addWorld);
+
+app.on('authenticated', authenticated);
+app.on('authenticated',  function(response){ app.service('worlds').find().then(worlds => console.log(worlds))});
+app.on('logout', loggedOut);
+app.on('reauthentication-error', login);
+
+login();
+
